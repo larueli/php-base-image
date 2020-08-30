65e14eb366598672910b516f5194653f4cfee5ab
Ivann LARUELLE <37900449+larueli@users.noreply.github.com>
2020-08-30 14:12:24 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-if [ ! -z ${DOCUMENT_ROOT} ]
+if [ ! -z ${APACHE_DOCUMENT_ROOT} ]
 then
 	sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf
 	sed -ri -e 's!/var/www/!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
