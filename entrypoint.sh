dde0aaa7ccd8c3d484132b27ceb9c15d0c2b0444
Ivann LARUELLE <ivann@laruelle.me>
2021-03-16 16:15:18 +0100

    fix : using double quotes for sed with APACHE_DOCUMENT_ROOT

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ ! -z ${APACHE_DOCUMENT_ROOT} ]
 then
-	sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf
-	sed -ri -e 's!/var/www/!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
+	sed -ri -e "s!/var/www/html!${APACHE_DOCUMENT_ROOT}!g" /etc/apache2/sites-available/*.conf
+	sed -ri -e "s!/var/www/!${APACHE_DOCUMENT_ROOT}!g" /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
 fi
 
 if [ -d /docker-entrypoint-init.d ]
